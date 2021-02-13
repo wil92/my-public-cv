dc9f2568f41e8b2b72b11c6e69736b9102f2b75c
Guillermo Gonzalez <ggjnez92@gmail.com>
2021-02-13 21:21:33 +0100

    Add subcontent with Profile and experience

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,26 +1,83 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 import {
   faFacebook,
   faTwitter,
-  faLinkedinIn
+  faLinkedinIn,
+  faXingSquare,
+  faStackOverflow
 } from '@fortawesome/free-brands-svg-icons';
+
+import {
+  faLaptop,
+  faGraduationCap,
+  faMicrophone
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'cv-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  faceIcon = faFacebook;
-  twitterIcon = faTwitter;
-  linkedinIcon = faLinkedinIn;
+  podcastIcon = faMicrophone;
+  laptopIcon = faLaptop;
+  educationIcon = faGraduationCap;
+
+  @ViewChild('actions_container')
+  actionsContainer: ElementRef;
+
+  actionHeight = 0;
+
+  socials = [
+    {
+      url: '#',
+      icon: faLinkedinIn,
+      label: 'LinkedIn'
+    },
+    {
+      url: '#',
+      icon: faTwitter,
+      label: 'Twitter'
+    },
+    {
+      url: '#',
+      icon: faXingSquare,
+      label: 'Xing'
+    },
+    {
+      url: '#',
+      icon: faStackOverflow,
+      label: 'StackOverflow'
+    },
+    {
+      url: '#',
+      icon: faFacebook,
+      label: 'Facebook'
+    }
+  ];
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.updateActionButtonsSize();
+  }
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.updateActionButtonsSize()
+  }
+
+  updateActionButtonsSize() {
+    const FIRST_CHILD = 0;
+    const actions = this.actionsContainer.nativeElement.children;
+    // noinspection JSSuspiciousNameCombination
+    this.actionHeight = actions[FIRST_CHILD].clientWidth;
   }
 
 }

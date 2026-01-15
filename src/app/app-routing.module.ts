b03e08ed253205081bc15fccb89510f5755cee50
guille <ggjnez92@gmail.com>
2026-01-15 18:26:54 +0100

    Update version to 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,8 +23,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-    onSameUrlNavigation: 'reload',
-    relativeLinkResolution: 'legacy'
+    onSameUrlNavigation: 'reload'
 })
   ],
   exports: [RouterModule]
